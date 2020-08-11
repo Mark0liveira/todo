599339f1da76ec608a887699035aa5353571cd77
Marcos de Oliveira <marcosdeoliveira@MacBook-Pro-de-Marcos.local>
2020-08-10 23:26:14 -0300

    projeto to do list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { Todo } from './models/todo.model';
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,83 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  public todos: Todo[] = [];
+  public title = 'TODO LIST';
+
+  public form;
+
+  public modal;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required
+      ])],
+    });
+
+    this.load();
+  }
+
+  public remove(todo: Todo): void {
+    const index = this.todos.indexOf(todo);
+
+    if (index !== -1) {
+      this.todos.splice(index, 1);
+    }
+
+    this.save();
+  }
+
+  public add(): void {
+    const title = this.form.controls.title.value;
+    const index = this.todos.length + 1;
+
+    this.todos.push(new Todo(index, title, false));
+
+    this.save();
+    this.clear();
+  }
+
+  public clear(): void {
+    this.form.reset();
+
+    this.save();
+  }
+
+  public markAsDone(todo: Todo): void {
+    todo.concluido = true;
+
+    this.save();
+  }
+
+  public markAsUndone(todo: Todo): void {
+    todo.concluido = false;
+
+    this.save();
+  }
+
+  public save(): void {
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
+
+    this.modal = false;
+  }
+
+  public load(): void {
+    const data = localStorage.getItem('todos');
+
+    if (data) {
+      this.todos = JSON.parse(data);
+      this.modal = false;
+    } else {
+      this.todos = [];
+      this.modal = true;
+    }
+  }
+
+  public addTarefa(): void {
+    this.modal = true;
+  }
 }
